c36db11cb4b6b22836061280a230772f477e805f
ggondo <gondo.g@aol.com>
2023-04-29 01:49:38 -0300

    Apostila 1 de React Native

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { 
+  Button,
+  StyleSheet,
+  TextInput, 
+  Text, 
+  View } from 'react-native';
 
 export default function App() {
+  const [lembrete, setLembrete] = useState('')
+  const [lembretes, setLembretes] = useState([])
+  const capturarLembrete = (lembreteDigitado) => {
+    setLembrete(lembreteDigitado)
+  }
+    const adicionarLembrete = () => {
+      setLembretes(lembretes => {
+        const aux = [lembrete, ...lembretes]
+        setLembrete('')
+        return aux
+      })
+    }
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput 
+        style={styles.lembreteTextInput}
+        placeholder='Desejo Lembrar ..'
+        onChangeText={capturarLembrete}
+        value={lembrete}
+        />
+        <Button
+          title="OK"
+          onPress={adicionarLembrete} />
+      </View>
+      <View>
+        lembretes.map((l) => <View><Text>{l}</Text></View>)
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
+    borderColor: 'blue',
+    borderWidth: 1,
+    width: '100%'
   },
-});
+  lembreteTextInput:{
+    borderBottomColor: '#CCC',
+    borderBottomWidth: 1,
+    padding: 12,
+    textAlign: 'center',
+    outlineStyle: 'none',
+    marginBottom: 4
+  },
+  itemNaLista:{
+    padding:12,
+    borderColor: '#EEE',
+    backgroundColor: '#DDD',
+    marginBottom: 4,
+    textAlign:'center',
+    borderRadius: 4
+  }
+})
+
